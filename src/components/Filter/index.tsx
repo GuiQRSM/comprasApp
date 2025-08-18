d19bd80f4bd44921d0662ff0287f8a81c5f375cb
GuiQRSM <quaresmagui20@gmail.com>
2025-08-18 11:18:43 -0300

    add: Implements lucide icons at the project

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -1,6 +1,7 @@
 import { TouchableOpacity, TouchableOpacityProps, Text } from 'react-native';
 import { styles } from './styles';
 import { FilterStatus } from '@/types/FilterStatus';
+import { CircleCheck, CircleDashed } from 'lucide-react-native';
 
 type ContentProps = TouchableOpacityProps & {
   status: FilterStatus;
@@ -10,10 +11,13 @@ type ContentProps = TouchableOpacityProps & {
 export function Content({ status, isActive, ...rest }: ContentProps) {
   return (
     <TouchableOpacity
+      //é possivel colocar mais de uma estilizaçõo em uma linha
       style={[styles.container, { opacity: isActive ? 1 : 0.5 }]}
       activeOpacity={0.8}
       {...rest}
     >
+      <CircleCheck size={15} color={'black'} />
+
       <Text style={styles.title}>{status === FilterStatus.DONE ? 'Comprados' : 'Pendentes'}</Text>
     </TouchableOpacity>
   );
