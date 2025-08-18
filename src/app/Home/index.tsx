df031d2c85c438571b42905b6c1d9a7636a7ffe9
GuiQRSM <quaresmagui20@gmail.com>
2025-08-18 09:53:49 -0300

    add: declare content at the main index

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -7,8 +7,13 @@ export function Home() {
   return (
     <View style={styles.container}>
       <Image source={require('@/assets/logo.png')} style={styles.logo} />
-      <Input placeHolder="O que você precisa comprar" />
-      <Button title="Entrar" activeOpacity={0.7} onPress={() => console.log('Entrar')} />
+
+      <View style={styles.form}>
+        <Input placeHolder="O que você precisa comprar?" />
+        <Button title="Entrar" activeOpacity={0.7} onPress={() => console.log('Entrar')} />
+      </View>
+
+      <View style={styles.content}></View>
     </View>
   );
 }
--- src/app/Home/styles.ts
+++ src/app/Home/styles.ts
@@ -4,13 +4,28 @@ import { colors } from '@/styles/colors';
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: colors.background.primary,
+    paddingTop: 62,
   },
   logo: {
     height: 34,
     width: 134,
-    paddingBottom: 8,
+  },
+  form: {
+    width: '100%',
+    alignItems: 'center',
+    paddingHorizontal: 16,
+    gap: 5,
+    marginTop: 25,
+  },
+  content: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: colors.white.main,
+    borderTopLeftRadius: 24,
+    borderTopRightRadius: 24,
+    padding: 24,
+    marginTop: 24,
   },
 });
